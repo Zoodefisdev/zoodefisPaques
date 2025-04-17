5e05b649bd635cce77eab2e64fde6ef559e9df08
Zoodefisdev <zoodefisdev@gmail.com>
2025-04-17 10:14:52 +0200

    Delete zoodefisPaques directory

--- zoodefisPaques/js/configs.js
+++
@@ -1,1 +0,0 @@
-export const ENDPOINT = "https://zoodefis.netlify.app/"; //changer avec app
--- zoodefisPaques/js/utils/Scorestotal.js
+++
@@ -1,4 +0,0 @@
-export default function getTotalQuestions() {
-    let questions = JSON.parse(localStorage.getItem("questions")) || [];
-    return questions.length;
-}
